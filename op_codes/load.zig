0b391a7bbf0701c8744bafb0b2b9ebfc39262537
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-04 19:49:46 -0600

    start defining load op codes

---
+++ op_codes/load.zig
@@ -0,0 +1,28 @@
+// Load/Store Operations
+
+//! These instructions transfer a single byte between memory and one of the registers. 
+//! Load operations set the negative (N) and zero (Z) flags depending on the value of transferred.
+//! Store operations do not affect the flag settings.
+
+// Load Accumulator
+// LDA 	Load Accumulator 	N,Z
+pub fn LDA (accumulator: CPU.accumulator, value: i8) {
+  if (value == 0) {
+      CPU.
+  }
+}
+
+// Load X Register
+// LDX 	Load X Register 	N,Z
+
+// Load Y Register
+// LDY 	Load Y Register 	N,Z
+
+// Store Accumulator
+// STA 	Store Accumulator
+
+// Store X Register
+// STX 	Store X Register
+
+// Store Y Register
+// STY 	Store Y Register 	 
