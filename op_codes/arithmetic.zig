2d62daece6cdff6f8668a5cc8051826bbaf1c316
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-05 22:44:45 -0600

    add arithmetic opcodes and start branch opcodes

---
+++ op_codes/arithmetic.zig
@@ -0,0 +1,18 @@
+
+// Arithmetic Operations
+
+//! The arithmetic operations perform addition and subtraction on the contents of the accumulator.
+//! The compare operations allow the comparison of the accumulator and X or Y with memory values.
+
+// ADD 	Add with Carry 	N,V,Z,C
+pub fn ADC () {
+}
+
+// SBC 	Subtract with Carry 	N,V,Z,C
+
+// CMP 	Compare Accumulator 	N,Z,C
+
+// CPX 	Compare X register      N,Z,C
+
+// CPY 	Compare Y register      N,Z,C
+
